583e6bc780abf754a25e2ca3170e1c6355091916
Tsmeholic <tsmeholic@gmail.com>
2023-03-15 00:00:01 -0400

    Edited main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const HOST = '0.0.0.0';
+const PORT = Number(process.env.PORT) || 8080
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 5000);
+  await app.listen(PORT, HOST).then(() => {
+    console.log("connected")
+  });
 }
 bootstrap();
