1e74c44ce90f613278b162eab59376db3b284a6d
Tsmeholic <tsmeholic@gmail.com>
2023-03-22 00:11:42 -0400

    removed mongoose module.

--- src/amandaskitchen/amandaskitchen.controller.ts
+++ src/amandaskitchen/amandaskitchen.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body, Put, Query, Delete } from '@nestjs/common';
-import { ValidateObjectId } from './shared/pipes/validate-object-id.pipes';
 import { AmandasKitchenService } from './amandaskitchen.service';
 import { Recipe } from './interfaces/recipe.entity';
 
@@ -19,7 +18,7 @@ export class AmandasKitchenController {
     }
 
     @Delete('/delete')
-    async deleteRecipe(@Res() res, @Query('recipeID', new ValidateObjectId()) recipeID) {
+    async deleteRecipe(@Res() res, @Query('recipeID') recipeID) {
         const deletedRecipe = await this.amandasKitchenService.deleteRecipe(recipeID);
         if (!deletedRecipe){
             throw new NotFoundException(recipeDNE);
@@ -31,7 +30,7 @@ export class AmandasKitchenController {
     }
 
     @Put('/edit')
-    async editRecipe(@Res() res, @Query('recipeID', new ValidateObjectId()) recipeID, @Body() addRecipeDTO: Recipe){
+    async editRecipe(@Res() res, @Query('recipeID') recipeID, @Body() addRecipeDTO: Recipe){
         const editedRecipe = await this.amandasKitchenService.editRecipe(recipeID, addRecipeDTO)
         if (!editedRecipe){
             throw new NotFoundException(recipeDNE);
@@ -43,7 +42,7 @@ export class AmandasKitchenController {
     }
     
     @Get('/recipe/:recipeID')
-    async getRecipe(@Res() res, @Param('recipeID', new ValidateObjectId()) recipeID){
+    async getRecipe(@Res() res, @Param('recipeID') recipeID){
         const recipe = await this.amandasKitchenService.getRecipe(recipeID);
         if (!recipe){
             throw new NotFoundException(recipeDNE);
